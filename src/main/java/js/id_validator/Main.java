2e99d76c76d2e21a4b5ba19bdf183e2982db0141
justyna <justyna.stanek.js@gmail.com>
2019-06-22 11:47:06 +0200

    add Main

--- src/main/java/js/id_validator/Main.java
+++ src/main/java/js/id_validator/Main.java
@@ -1,7 +1,23 @@
 package js.id_validator;
 
-public class Main {
+import picocli.CommandLine;
 
-    
+
+public class Main implements Runnable{
+
+    @CommandLine.Option(names = {"-id", "--id_number"}, required = true, description = "The user's ID number")
+    String idNumber;
+
+    public void run() {
+
+        ID_Validator my_id_validator = new ID_Validator();
+        my_id_validator.validate(idNumber);
+
+    }
+
+    public static void main(String... args) {
+        CommandLine.run(new Main(), System.err, args);
+    }
+
 
 }
